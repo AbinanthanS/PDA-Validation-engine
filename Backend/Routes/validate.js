bcd08e5d85b5d90bf2989d20e6b8786449d6ca7e
Abinanthan <s.a.abinanthan2005@gmail.com>
2025-11-06 10:53:58 +0530

    validate.js path fixed

--- Backend/Routes/validate.js
+++ Backend/Routes/validate.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const decrypt = require('../middleware/decrypt.js');
-const { validatePayload } = require('../controllers/validationController.js');
+const { validatePayload } = require('../Controllers/validationController.js');
 
 router.post('/', decrypt, validatePayload);
 
